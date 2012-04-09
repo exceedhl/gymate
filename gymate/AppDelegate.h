72966d6d2e5ef5f2384db1ca2a3aca4abf2f1300
Huang Liang <exceedhl@gmail.com>
2012-04-09 14:55:02 +1000

    Initial Commit

---
+++ gymate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gymate
+//
+//  Created by Liang Huang on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
