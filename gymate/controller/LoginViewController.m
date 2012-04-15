d4a7455a4cdbd3c6c9eda5e5a322dc396262a05a
Yuan Heng Fan <fanyuanheng@gmail.com>
2012-04-15 23:22:08 +1000

    Alert on login failure

--- gymate/controller/LoginViewController.m
+++ gymate/controller/LoginViewController.m
@@ -25,15 +25,17 @@
     dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
         User *user = [User logInWithEmail:self.email.text password:self.password.text];
         dispatch_async(dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideHUDForView:self.view animated:NO];
             if (user) {
                 NSLog(@"User: %@", user);
                 NSLog(@"session token: %@", user.sessionToken);
                 NSLog(@"new user?: %d", user.isNew);
                 NSLog(@"Current User: %@", [User currentUser]);
             } else {
-                hud.labelText = @"Login failed...";
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Login Failed" message:@"Please try again." delegate:self cancelButtonTitle:@"Close" otherButtonTitles:nil, nil];
+                [alert show];
             }
-            [MBProgressHUD hideHUDForView:self.view animated:YES]; 
+             
         });
     });
 
